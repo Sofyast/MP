32935d8996c63314f3923c6501b7f126f221d16e
Sofyast <55927955+Sofyast@users.noreply.github.com>
2022-09-28 16:26:44 +0300

    Update SetImpl.java

--- main/java/SetImpl.java
+++ main/java/SetImpl.java
@@ -1,30 +1,35 @@
-import java.util.ArrayList;
+import java.util.ArrayList;                                     //curr - текущий, pred - предыдущий, value - значение ключа
 
-public class SetImpl<T extends Comparable<T>> implements Set<T> {
+public class SetImpl<T extends Comparable<T>> implements Set<T> {        // ?
 
-    public final Node<T> startt;
+    //    создание пустого списка, сотоящего из 2-x граничных элементов
+    public final Node<T> startt;         
     public final Node<T> konez;
 
     public SetImpl() {
-        startt = new Node<>(null, null);
+        startt = new Node<>(null, null);        // ?
         konez = new Node<>(null, startt);
     }
 
     public ArrayList<Node<T>> find(T value) {
         ArrayList<Node<T>> arrayN = new ArrayList<Node<T>>(2);
+    
         find_again:
+   
         while (true) {
             Node<T> pred = konez;
-            Node<T> curr = konez.next.getReference();
+            Node<T> curr = konez.next.getReference();            // возвращает текущее значение ссылки
+           
             while (true) {
                 boolean[] cmk = {false};
                 Node<T> succ = curr.next.get(cmk);
                 if (cmk[0]) {
-                    if (pred.next.compareAndSet(curr, succ, false, false)) {
+                    if (pred.next.compareAndSet(curr, succ, false, false)) {    
                         continue find_again;
                     }
                     curr = succ;
                 } else {
+              
                     if (curr.item == null || curr.item.compareTo(value) >= 0) {
                         arrayN.add(pred);
                         arrayN.add(curr);
@@ -40,7 +45,7 @@ public class SetImpl<T extends Comparable<T>> implements Set<T> {
 
 
     @Override
-    public boolean add(T value) {
+    public boolean add(T value) {    //добавить ключ к множеству
         while (true) {
             ArrayList<Node<T>> arrayN = find(value);
             Node<T> pred = arrayN.get(0);
@@ -58,7 +63,7 @@ public class SetImpl<T extends Comparable<T>> implements Set<T> {
     }
 
     @Override
-    public boolean remove(T value) {
+    public boolean remove(T value) {        //удалить ключ из множества
         while (true) {
             ArrayList<Node<T>> arrayN = find(value);
             Node<T> pred = arrayN.get(0);
@@ -77,7 +82,7 @@ public class SetImpl<T extends Comparable<T>> implements Set<T> {
     }
 
     @Override
-    public boolean contains(T value) {
+    public boolean contains(T value) {             //проверка наличия ключа в множестве
         Node<T> curr = konez.next.getReference();
         while (curr.item != null && curr.item.compareTo(value) < 0) {
             curr = curr.next.getReference();
@@ -86,7 +91,7 @@ public class SetImpl<T extends Comparable<T>> implements Set<T> {
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty() {            //проверка множества на пустоту
         while (konez.next.getReference() != startt) {
             Node<T> curr = konez.next.getReference();
             if (!curr.next.isMarked()) {
