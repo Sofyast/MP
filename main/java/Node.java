d4281ac21606f58c385becfe0a4ae7a04eee0618
Sofyast <55927955+Sofyast@users.noreply.github.com>
2022-09-29 12:59:09 +0300

    Update Node.java

--- main/java/Node.java
+++ main/java/Node.java
@@ -2,11 +2,11 @@ import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class Node<T extends Comparable<T>> {      //?
     T item;
-    AtomicMarkableReference<Node<T>> next;      //?
+    AtomicMarkableReference<Node<T>> next;      //объеденим ссылку на следующий элемент списка и метку о том, что узел удален в одно поле
 
 
     public Node(T item, Node<T> next) {
         this.item = item;
-        this.next = new AtomicMarkableReference<>(next, false);     //?
-    }
-}
+        this.next = new AtomicMarkableReference<>(next, false);    //Замена ссылки будет производиться следующим образом: сравнивается первый аргумент
+    }                                                              //с действительной ссылкой и то, что флаг по-прежнему равен false, и в случае успеха 
+}                                                                  //ссылка заменяется вторым аргументом. 
