23ca8e7491f82e6a136cddad0b4870867b98f600
Sofyast <55927955+Sofyast@users.noreply.github.com>
2022-09-29 13:05:40 +0300

    Update Node.java

--- main/java/Node.java
+++ main/java/Node.java
@@ -5,7 +5,7 @@ public class Node<T extends Comparable<T>> {      //?
     AtomicMarkableReference<Node<T>> next;      //объеденим ссылку на следующий элемент списка и метку о том, что узел удален в одно поле
 
 
-    public Node(T item, Node<T> next) {
+    public Node(T item, Node<T> next) {          //item?
         this.item = item;
         this.next = new AtomicMarkableReference<>(next, false);    //Замена ссылки будет производиться следующим образом: сравнивается первый аргумент
     }                                                              //с действительной ссылкой и то, что флаг по-прежнему равен false, и в случае успеха 
